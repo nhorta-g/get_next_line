6f552ae09ad5e62863b71c12a28278c655ea9f40
nhorta-g <101281946+nhorta-g@users.noreply.github.com>
2022-04-14 19:57:01 +0100

    Add files via upload

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/14 19:07:41 by nhorta-g          #+#    #+#             */
-/*   Updated: 2022/04/14 19:45:32 by nhorta-g         ###   ########.fr       */
+/*   Created: 2022/04/14 19:08:26 by nhorta-g          #+#    #+#             */
+/*   Updated: 2022/04/14 19:50:55 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"	
+#include "get_next_line_bonus.h"	
 
 static char	*function_name(int fd, char *buf, char *backup)
 {
@@ -62,19 +62,19 @@ char	*get_next_line(int fd)
 {
 	char		*line;
 	char		*buf;
-	static char	*backup;
+	static char	*backup[OPEN_MAX];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (0);
-	line = function_name(fd, buf, backup);
+	line = function_name(fd, buf, backup[fd]);
 	free(buf);
 	buf = NULL;
 	if (!line)
 		return (NULL);
-	backup = extract(line);
+	backup[fd] = extract(line);
 	return (line);
 }
 /*
@@ -82,17 +82,29 @@ int	main(void)
 {
 	char	*line;
 	int		i;
+	int		fd1;
 	int		fd2;
+	int		fd3;
+	fd1 = open("tests/test.txt", O_RDONLY);
 	fd2 = open("tests/test2.txt", O_RDONLY);
+	fd3 = open("tests/test3.txt", O_RDONLY);
 	i = 1;
-	while (i < 6)
+	while (i < 7)
 	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
 		line = get_next_line(fd2);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd3);
 		printf("line [%02d]: %s", i, line);
 		free(line);
 		i++;
 	}
+	close(fd1);
 	close(fd2);
+	close(fd3);
 	return (0);
 }
 */
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/14 19:08:42 by nhorta-g          #+#    #+#             */
-/*   Updated: 2022/04/14 19:42:01 by nhorta-g         ###   ########.fr       */
+/*   Created: 2022/04/14 19:23:45 by nhorta-g          #+#    #+#             */
+/*   Updated: 2022/04/14 19:41:57 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <limits.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strlen(const char *str);
